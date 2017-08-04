0442421b9de3db86d59585ba4ce068cac8b916b7
Roman <sg.romanmazurets@gmail.com>
2017-08-04 14:11:04 +0300

    add place, move, left, right, report methods

--- robot.rb
+++ robot.rb
@@ -1,21 +1,63 @@
 class Robot
   SQUARE_LIMIT = 5
-  @x = *(0..4)
-  @y = *(0..4)
+  DIRECTIONS = %i{north east south west}
+  X = *(0..4)
+  Y = *(0..4)
 
-  attr_reader :x, :y
+  @x = nil
+  @y = nil
+  @facing = nil
+
+  attr_reader :x, :y, :facing
 
   def initialize(x=0, y=0)
     @x = x
     @y = y
   end
 
-  def place(x, y)
-    @x = x
-    @y = y
+  def place(x=@x, y=@y, f)
+    @x = x if X.include?(x)
+    @y = y if Y.include?(y)
+    @facing = f if DIRECTIONS.include?(f)
+  end
+
+  def report
+    {x: @x, y: @y, facing: @facing}
+  end
+
+  def move
+    case @facing
+      when :north
+        @y -= 1 if Y.include?(@x - 1)
+      when :south
+        @y += 1 if Y.include?(@x + 1)
+      when :east
+        @x += 1 if X.include?(@x + 1)
+      when :west
+        @x -= 1 if X.include?(@x - 1)
+    end
+  end
+
+  def left
+    # case @facing
+    #   when :north
+    #     @facing = :west
+    #   when :south
+    #     @facing = :east
+    #   when :east
+    #     @facing = :north
+    #   when :west
+    #     @facing = :south
+    # end
+    @facing = DIRECTIONS[(DIRECTIONS.index(@facing) - 1) % 4]
+  end
+
+  def right
+    @facing = DIRECTIONS[(DIRECTIONS.index(@facing) + 1) % 4]
   end
 
   def hello
     "robot"
   end
 end
+
--- robot_test.rb
+++ robot_test.rb
@@ -1,6 +1,5 @@
 require './robot'
 require 'minitest/autorun'
-require 'minitest/pride'
 
 class TestRobot < Minitest::Test
   def setup
@@ -12,9 +11,53 @@ class TestRobot < Minitest::Test
   end
 
   def test_place
-    @robot.place(1, 2)
+    @robot.place(1, 2, :west)
     assert_equal @robot.x, 1
     assert_equal @robot.y, 2
+    assert_equal @robot.facing, :west
+  end
+
+  def test_place_with_wrong_facing
+    @robot.place(1, 2, :aaa)
+    assert_equal @robot.x, 1
+    assert_equal @robot.y, 2
+    assert_nil @robot.facing
+  end
+
+  def test_place_with_wrong_position
+    @robot.place(5, 6, :west)
+    assert_equal @robot.x, 0
+    assert_equal @robot.y, 0
+    assert_equal @robot.facing, :west
+  end
+
+  def test_report
+    @robot.place(1, 2, :east)
+    assert_equal @robot.report, {x: 1, y: 2, facing: :east}
+  end
+
+  def test_move
+    @robot.place(f=:east)
+    @robot.move
+    assert_equal @robot.report, {x:1, y:0, facing: :east}
+  end
+
+  def test_out_of_range_move
+    @robot.place(4, 0,  :east)
+    @robot.move
+    assert_equal @robot.report, {x:4, y:0, facing: :east}
+  end
+
+  def test_left
+    @robot.place(0,0,:north)
+    @robot.left
+    assert_equal @robot.facing, :west
+  end
+
+  def test_right
+    @robot.place(0,0,:west)
+    @robot.right
+    assert_equal @robot.facing, :north
   end
 
 end
